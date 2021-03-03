f818992ab93d451ba45773e3bb87a0085939ac28
Alex Devero <deveroalex@gmail.com>
2021-03-03 13:00:38 +0100

    Add footer component

---
+++ src/components/app-footer.tsx
@@ -0,0 +1,23 @@
+export const AppFooter = () => (
+  <footer>
+    <ul className="list-unstyled d-flex justify-content-center">
+      <li className="mr-4">
+        <a href="https://github.com/alexdevero/tweeterize-web-app" target="_blank" rel="noopener noreferrer" className="text-white">
+          <span className="small bi bi-github" />
+        </a>
+      </li>
+
+      <li className="mr-4">
+        <a href="https://twitter.com/AlexDevero" target="_blank" rel="noopener noreferrer" className="text-white">
+          <span className="small bi bi-twitter" />
+        </a>
+      </li>
+
+      <li>
+        <a href="https://devero.io" target="_blank" rel="noopener noreferrer" className="text-white">
+          <span className="small bi bi-globe" />
+        </a>
+      </li>
+    </ul>
+  </footer>
+)
