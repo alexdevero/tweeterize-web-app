6397b179b8d6eebc3dec7b7d9cfcab5893160279
Alex Devero <deveroalex@gmail.com>
2021-03-04 13:51:42 +0100

    Restyle messages

--- src/components/app-messages.tsx
+++ src/components/app-messages.tsx
@@ -13,11 +13,11 @@ export const AppMessages = (props: AppMessagesUI) => {
       <h2 className="h5 mb-4">Your Formatted messages:</h2>
 
       {props.messages.map((el, i) => {
-        return <div className={`d-flex justify-content-between align-items-start ${props.messages.length !== 1 && i < props.messages.length - 1 ? 'mb-4' : ''}`} key={i}>
-          <span className="h6 font-weight-normal w-100 pr-2">{el}</span>
+        return <div className={`d-flex justify-content-between align-items-start bg-white p-3 text-body rounded ${props.messages.length !== 1 && i < props.messages.length - 1 ? 'mb-4' : ''}`} key={i}>
+          <span className="h6 font-weight-normal w-100 pr-2 m-0">{el}</span>
 
           <CopyToClipboard text={el} onCopy={() => props.handleSetNotificationVisible()}>
-            <button className="btn btn-light btn-sm" title="Copy text to clipboard">
+            <button className="btn btn-secondary btn-sm" title="Copy text to clipboard">
               <span className="bi bi-clipboard"></span>
             </button>
           </CopyToClipboard>
