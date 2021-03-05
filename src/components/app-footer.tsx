ddd4d5c29c27d5ccbcccd4d929b0678b015ffdbe
Alex Devero <deveroalex@gmail.com>
2021-03-05 11:31:08 +0100

    Add new links to footer

--- src/components/app-footer.tsx
+++ src/components/app-footer.tsx
@@ -1,5 +1,5 @@
 export const AppFooter = () => (
-  <footer>
+  <footer className="pb-3">
     <ul className="list-unstyled d-flex justify-content-center">
       <li className="mr-4">
         <a href="https://github.com/alexdevero/tweeterize-web-app" target="_blank" rel="noopener noreferrer" className="text-white">
@@ -13,11 +13,25 @@ export const AppFooter = () => (
         </a>
       </li>
 
-      <li>
+      <li className="mr-4">
         <a href="https://devero.io" target="_blank" rel="noopener noreferrer" className="text-white">
           <span className="small bi bi-globe" />
         </a>
       </li>
+
+      <li className="mr-4">
+        <a href="https://paypal.me/alexdevero" target="_blank" rel="noopener noreferrer" className="text-white">
+          <span className="small bi bi-wallet2" />
+        </a>
+      </li>
+
+      <li>
+        <a href="https://buymeacoffee.com/alexdevero" target="_blank" rel="noopener noreferrer" className="text-white">
+          <span className="small bi bi-cup-straw" />
+        </a>
+      </li>
     </ul>
+
+    <p className="h6 font-weight-normal text-center pt-1">Created by <a href="https://devero.io" target="_blank" rel="noopener noreferrer" className="text-white">Alex Devero</a>, <a href="https://devero.io" target="_blank" rel="noopener noreferrer" className="text-white">Devero Corp.</a></p>
   </footer>
 )
